ae3fe7a84a038d7d7c09808d459dd7f58e8f2ce3
INWYTIA <inwytia@gmail.com>
2020-05-12 03:16:50 -0500

    table trouble shoot

--- src/API/employees.js
+++ src/API/employees.js
@@ -1,19 +1,19 @@
-const employees = {
-  0: {
+const employees = [
+  {
     name: 'George',
     department: 'EVS',
     type: 'FT'
   },
-  1: {
+  {
     name: 'Michael',
     department: 'Security',
     type: 'FT'
   },
-  2: {
+  {
     name: 'Brad',
     department: 'Marketing',
     type: 'PT'
   }
-}
+]
 
 export default employees;
